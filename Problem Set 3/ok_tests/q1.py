2f6f8ef24c9d6653dfd8773f4d9444d5a1237491
ronald.walker <ronald.walker@berkeley.edu>
2017-11-06 03:57:37 +0000

    ok / clearing outputs

---
+++ Problem Set 3/ok_tests/q1.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'No tests for this problem set',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # can put comments here
+          >>> 4 == 4
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
